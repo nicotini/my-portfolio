6de702681963ea323e95fa52e250b8ebd9ac43b9
nicotini <elenafreim@gmail.com>
2022-12-14 12:38:01 +0100

    brought out education and experience info at client part of the website

--- resources/js/components/pages/home/index.vue
+++ resources/js/components/pages/home/index.vue
@@ -162,7 +162,7 @@
                     </div>
                     </div>
                     </div>
-                    <div class="timeline__event  animated fadeInUp delay-3s timeline__event--type1" v-for="work in placesToWork" :key="work.id">
+                    <div class="timeline__event  animated fadeInUp delay-3s timeline__event--type3" v-for="work in placesToWork" :key="work.id">
                         <div class="timeline__event__icon ">
                             <i class="lni-cake"></i>
                             <div class="timeline__event__date">
@@ -182,53 +182,17 @@
             </section>
 
             <!--==================== SERVICES ====================-->
-            <section class="services section" id="services">
+            <section class="services section" id="services" v-if="services">
               <h2 class="section__title">Services</h2> 
               <span class="section__subtitle">What is offer</span> 
 
               <div class="services_container container grid">
+                <template v-for="service in services" :key="service.id">
                 <!--============== SERVICES 1 =============-->
                 <div class="services_content">
                     <div>
                         <i class="uil uil-web-grid services_icon"></i>
-                        <h3 class="services_title">Ui/Ux <br> Designer</h3>
-                    </div>
-                    <span class="button button--flex button--small button--link services_button">
-                        View More
-                        <i class="uil uil-arrow-right button_icon"></i>
-                    </span>
-
-                    <div class="services_modal ">
-                        <div class="services_modal-content">
-                            <h4 class="services_modal-title">UI/UX <br> Designer</h4>
-                            <i class="uil uil-times services_modal-close"></i>
-
-                            <ul class="services_modal-services grid">
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I develop the user interface</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>Web page development</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I create ux element interactions</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I position your company brand.</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!--============== SERVICES 2 =============-->
-                <div class="services_content">
-                    <div>
-                        <i class="uil uil-web-grid services_icon"></i>
-                        <h3 class="services_title">Frontend <br> Developer</h3>
+                        <h3 class="services_title">{{ service. title }}</h3>
                     </div>
                     <span class="button button--flex button--small button--link services_button">
                         View More
@@ -237,7 +201,7 @@
 
                     <div class="services_modal">
                         <div class="services_modal-content">
-                            <h4 class="services_modal-title">Frontend <br> Developer</h4>
+                            <h4 class="services_modal-title">{{ service.title }}</h4>
                             <i class="uil uil-times services_modal-close"></i>
 
                             <ul class="services_modal-services grid">
@@ -261,80 +225,7 @@
                         </div>
                     </div>
                 </div>
-                <!--============== SERVICES 3 =============-->
-                <div class="services_content">
-                    <div>
-                        <i class="uil uil-web-grid services_icon"></i>
-                        <h3 class="services_title">Backend <br> Developer</h3>
-                    </div>
-                    <span class="button button--flex button--small button--link services_button">
-                        View More
-                        <i class="uil uil-arrow-right button_icon"></i>
-                    </span>
-
-                    <div class="services_modal">
-                        <div class="services_modal-content">
-                            <h4 class="services_modal-title">Backend <br> Developer</h4>
-                            <i class="uil uil-times services_modal-close"></i>
-
-                            <ul class="services_modal-services grid">
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I develop the user interface</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>Web page development</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I create ux element interactions</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I position your company brand.</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!--============== SERVICES 4 =============-->
-                <div class="services_content">
-                    <div>
-                        <i class="uil uil-web-grid services_icon"></i>
-                        <h3 class="services_title">Branding <br> Designer</h3>
-                    </div>
-                    <span class="button button--flex button--small button--link services_button">
-                        View More
-                        <i class="uil uil-arrow-right button_icon"></i>
-                    </span>
-
-                    <div class="services_modal">
-                        <div class="services_modal-content">
-                            <h4 class="services_modal-title">Branding <br> Designer</h4>
-                            <i class="uil uil-times services_modal-close"></i>
-
-                            <ul class="services_modal-services grid">
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I develop the user interface</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>Web page development</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I create ux element interactions</p>
-                                </li>
-                                <li class="services_modal-service">
-                                    <i class="uil uil-check-circle services_modal-icon"></i>
-                                    <p>I position your company brand.</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                </template>
               </div>
             </section>
 
